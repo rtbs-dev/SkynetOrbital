8f7ebd2e1ee4462a0014724c012758ba903184ac
antantantant <renyi.tsinghua@gmail.com>
2015-07-24 09:27:47 -0700

    edited simulation to perform steps
    need numbers for state and action space definition

--- simulation.py
+++ simulation.py
@@ -1,5 +1,5 @@
 __author__ = 'Thurston Sexton, Max Yi Ren'
-# This file defines the underlying physics for project SkynetOrbital
+# This file defines the underlying physics
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -7,110 +7,111 @@ from scipy.integrate import odeint
 
 class Simulation:
 
-    G = 6.672e-11  # Gravitational Constant
-    # M = 5.97219e24 # Mass of Earth
-    M = 5.2915793e22  # Mass of Kerbin
-    R = 6e5  # Radius of Kerbin
-    # R= 6.378e6  # mean Radius of Earth
-
     def __init__(self, design):
-        # TODO: define state, action spaces and initial state for this particular simulation
-        self.state_space = []
-        self.action_space = []
-        self.initial_state = []
-
-        # design variables
-        self.mf0 = design.mf0
-        self.burn = design.burn
-
         # self.mf0 = input('How much fuel (kg) to bring along (can change later)?')
         # print 'setting burn scheme (call change_design() to change)'
         # self.burn = b  # a piecewise func determining throttle @ some time t
 
         # design parameters
         self.G = 6.672e-11
-        self.M_k = 5.2915793e22 # Mass of Kerbin
-        self.R_k = 6e5 # Radius of Kerbin
+        self.M = 5.2915793e22 # Mass of Kerbin
+        # M = 5.97219e24 # Mass of Earth
+        self.R = 6e5  # Radius of Kerbin
+        # R= 6.378e6  # mean Radius of Earth
+        self.F = 189e5  # Thrust of the Engine, in N
+        self.m0 = 13e4  # Weight of empty rocket, in kg
+        self.mf = self.design.mf0 # initial fuel mass
+        self.V0 = 6.1e2  # initial speed
+        self.gam0 = 1.5  # initial angle
+        self.r_0 = self.R+2.86875e2  # initial height
+        # self.phi0 = -1.5  # ???
+        self.time_interval = 1  # second
+        self.Isp = 304  # sec ???
+
+        # TODO: define state, action spaces and initial state for this particular simulation
+        self.state_space = np.array([[],[],[],[]]) # speed, angle, height, fuel mass
+        self.action_space = np.arange(0)  # burn rate
+        self.initial_state = np.array([self.V0, self.gam0, self.r_0, self.design.mf0])
+        self.current_state = self.initial_state
+        self.trajectory = np.empty((0, self.initial_state.size))
+        self.action = 0
+        self.design = design
 
     # change design
     def change_design(self, design):
-
-        self.mf0 = design.mf0  #new init. fuel
-        self.burn = design.b  # a piecewise func determining throttle @ some time t
+        self.design = design
+        # self.design.burn = design.b  # a piecewise func determining throttle @ some time t
 
     # check if should stop
-    # TODO:
     def running(self):
-        return True
+        # if fuel and no crash, continue
+        if self.design.mf0 > 0 and self.trajectory[-1, 2] >= self.R:
+            return True
+        else:
+            return False
 
-    # TODO: comment all functions
-    def T_w(self,t,g):  #thrust:weight ratio, based on S-IC
+    # thrust:weight ratio, based on S-IC
+    def T_w(self, g):
+            if self.mf > self.action*self.time_interval: # if enough fuel to burn
+                m_t = self.m0 + self.mf
+                return self.action*g*self.Isp/(m_t*g)  # ???
+            else:
+                return 0.
 
-            F = 189e5  # Thrust of the Engine, in N
-
-            m0 = 13e4  # Weight of empty rocket, in kg
-
-            #mf0 = 229e4 - m0 # Mass of initial fuel, in kg
-            mf0 = self.mf0 #inital fuel
-            #dmf = 80. #kg/s
-            dmf = self.burn(t) #already burned fuel
-
-            m_t = m0 + (mf0-dmf)
-
-            if m_t<=m0:  # or (t>405) # 405s only for original case
-                F=0.
-                #print 'Fuel is gone'
-            #print F/(m_t*g)
-            return F/(m_t*g)
-
-
-
-    def g(self,y,t):
-
+    # gravity turn
+    def g(self, y, t):
         V_i = y[0]
-        gam_i=y[1]
+        gam_i = y[1]
         r_i = y[2]
-        phi_i = y[3]
+        # phi_i = y[3]
 
-        assert r_i>=self.R, 'You have crashed'
+        assert r_i >= self.R, 'You have crashed'
         g = self.G*self.M/r_i**2
-        f_v = -g*(np.sin(gam_i) - self.T_w(t,g))
+        f_v = -g*(np.sin(gam_i) - self.T_w(g))
         f_gam=(V_i/r_i - g/V_i)*np.cos(gam_i) # + drag/ ortho. term
         f_r = V_i*np.sin(gam_i)
-        f_phi = (V_i/r_i)*np.cos(gam_i)
-        #print t
-        #print gam_i
-        return [f_v, f_gam, f_r, f_phi]
+        f_mf = -self.action
+        # f_phi = (V_i/r_i)*np.cos(gam_i)
+        # return [f_v, f_gam, f_r, f_phi]
+        return f_v, f_gam, f_r, f_mf
 
-    V0= 6.1e2
-    gam0 = 1.5
-    r_0 = R+2.86875e2
-    phi0 = -1.5
-    g0 = [V0, gam0, r_0, phi0]
-    t=np.linspace(0,5000,10000)
-
-    # TODO: put state, action and design into the problem formulation
+    # simulate one step (self.time_interval)
     def step(self, state, action, design):
-        soln=odeint(self.g, self.g0, self.t)
-        return soln
+        self.current_state = state
+        self.action = action
+        self.design = design
+        soln = odeint(self.g, self.current_state,
+                      np.arange(0, self.time_interval, 10))
+        self.trajectory = np.dstack((self.trajectory, soln))
+        new_state = soln[:,-1]
 
-    def plot(self, soln):
-        fig,ax = plt.subplots(nrows=1, ncols=3, figsize=(12,4))
+        v = new_state[0]
+        gamma = new_state[1]
+        r = new_state[2]
+        mf = new_state[3]
+        # reward = -(gamma/np.pi)**2 - (mf/self.design.mf0)**2 - (1 - self.G*self.M/r/v**2)**2
+        reward = -(gamma/np.pi)**2 - (1 - self.G*self.M/r/v**2)**2
+        return new_state, reward
+
+    # TODO: animation?
+    def plot(self):
+        soln = self.trajectory
+        fig, ax = plt.subplots(nrows=1, ncols=3, figsize=(12,4))
         for i,n in enumerate(ax.flatten()[:2]):
-            n.plot(t,soln[:,i])
-        ax.flatten()[-1].plot(t,soln[:,2]-R_earth)
+            n.plot(t, soln[:,i])
+        ax.flatten()[-1].plot(t,soln[:,2]-self.R)
 
-        fig=plt.figure(figsize=(10,10))
+        # fig=plt.figure(figsize=(10,10))
         ax1 = plt.subplot(111, polar=True)
         ax1.plot(soln[:,3], soln[:,2], linewidth=2)
-        ax1.plot(np.linspace(0,2*np.pi, 10000),R*np.ones(10000), linewidth=3)
+        ax1.plot(np.linspace(0,2*np.pi, 10000),self.R*np.ones(10000), linewidth=3)
         ax1.fill_betweenx(R*np.ones(10000), np.linspace(0,2*np.pi, 10000), color='g')
         ax1.set_ylim(1e4,1e6)
 
-        circle1=plt.Circle((0,0),R,color='y')
+        circle1=plt.Circle((0,0),self.R,color='y')
 
-        x=np.multiply(soln[:,2], np.cos(soln[:,3]))
-        y=np.multiply(soln[:,2], np.sin(soln[:,3]))
+        x=np.multiply(soln[:,2], np.cos(soln[:, 3]))
+        y=np.multiply(soln[:,2], np.sin(soln[:, 3]))
         plt.plot(x, y)
         plt.xlim(0e5,2e5)
         plt.ylim(-7.5e5, -5.5e5)
