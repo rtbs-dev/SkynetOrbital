97fc2c3f4e9f0c596f26cf81d3541764b03e2573
antantantant <renyi.tsinghua@gmail.com>
2015-07-26 22:11:48 -0700

    Bayesian optimization works but need to define burn rate stages
    still need to think about a good Q value model.

--- control.py
+++ control.py
@@ -1,15 +1,22 @@
 __author__ = 'Thurston Sexton, Max Yi Ren'
 import qlearning
+import directpolicy
 
 class Controller:
     def __init__(self, arg):
-        self.control_alg = arg.control_alg
+        self.control_alg = arg['control_alg']
         if self.control_alg == 'qlearning':
             self.controller = qlearning.QLearning(arg)
-        else:
-            try_other_method = 1
+        elif self.control_alg == 'bayesian':
+            self.controller = directpolicy.DirectPolicySearch(arg)
+
+        # check hand-tuned solution
+        # self.controller.obj(13000,500,0,0,0)
+        # self.controller.simulate([13000,500,0,0,0])
+
         self.controller.learn()
-        self.design = arg.desgin # for pickle
+        self.controller.simulate(self.controller.control_parameter.res['max'])
 
     def optimal_act(self, state, design):
-        return self.controller.e_greedy(state)
+        if self.control_alg == 'qlearning':
+            return self.controller.e_greedy(state)
